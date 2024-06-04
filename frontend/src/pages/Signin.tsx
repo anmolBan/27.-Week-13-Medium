acede6f83e63f2a2f53dd23ba4756f025ab40014
Anmol Bansal <bansal.anmol98@gmail.com>
2024-06-05 02:25:54 +0530

    Update: Added signin page

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,5 +1,19 @@
-function Signin(){
-    return <div>Sign in</div>
+import Auth from "../components/Auth"
+import { Quote } from "../components/Quote"
+
+function Signup(){
+    return (
+        <div>
+            <div className="grid grid-cols-1 lg:grid-cols-2">
+                <div>
+                    <Auth type="signin"/>
+                </div>
+                <div className="hidden lg:block">
+                    <Quote/>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default Signin;
+export default Signup
